dc69365ac3377e0104cbbcf7ded9d15dbea578bb
lenny <Lenmor LD>
2018-07-23 23:49:58 -0400

    add image and rating stars to PlaceDetailInfo

--- TODO.md
+++ TODO.md
@@ -1,3 +1,11 @@
+[/] add place detail, such as image and rating stars
+
+[ ] show top comment (or show a comment iterator, by querying place details)
+    -> can also have a gallery of pictures
+
+
+[ ]
+
 [ ] introduce basic routing: home, about, contact page
 
 [ ] graphql
--- dev/js/components/PlaceDetailInfo.js
+++ dev/js/components/PlaceDetailInfo.js
@@ -1,6 +1,18 @@
 import React from 'react';
 
-const PlaceDetailInfo = ({place}) => {
+class PlaceDetailInfo extends React.Component {
+
+    intToList(x) {
+      const a = [];
+      for(var i = 0; i < x; i++) {
+         a.push(i);
+      }
+      return a;
+    }
+
+    render() {
+
+      const { place } = this.props;
 
     if (!place) {
         return (<div>Preparing poutine =P...</div>);
@@ -17,11 +29,35 @@ const PlaceDetailInfo = ({place}) => {
     }
 
     return(
-        <div className="place-detail-info">
+        <div className="place-detail-info" >
             <h2>{place.name}</h2>
             <h3>{place.location.address1}</h3>
+            <div className="place-detail-info-container">
+              <img src={place.image_url} alt={place.name} />
+
+              <div className="rating">
+
+                {
+                    this.intToList(Math.floor(place.rating)).map(e => {
+                      return <span className="fa fa-star checked"></span>;
+                    })
+                }
+
+                {
+                    this.intToList(5 - Math.floor(place.rating)).map(e => {
+                      return <span className="fa fa-star"></span>;
+                    })
+                }
+
+              </div>
+            </div>
+
+
+
         </div>
     )
+
+  }
 }
 
 export default PlaceDetailInfo;
